c65afd3e36a26032ec53e76f08b0a50e7cf61bd4
tol00p <tomaslopes2001@gmail.com>
2022-02-08 23:33:18 +0000

    merge

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+//import {GUI} from 'dat.gui';
+var hasPanel = false;
+
 function endThreeJs(){
     //function ends three js animation so they dont stack
     //document.getElementById('canvas').innerHTML="";
@@ -16,6 +20,7 @@ function bigImage(smallImg)
 //threejs 
 
 function beginThreeJs(){
+
     endThreeJs();
     //var fullImg = document.getElementById("imageBox");
     const canvas = document.getElementById('canvas');
@@ -24,16 +29,24 @@ function beginThreeJs(){
     CANVAS_WIDTH = document.getElementById("canvas").offsetWidth,
     CANVAS_HEIGHT = document.getElementById("canvas").offsetHeight;
 
-    var scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x5f6661)
-    var camera = new THREE.PerspectiveCamera( 60, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 1000 )
-    var renderer = new THREE.WebGLRenderer()
-    var controls = new THREE.OrbitControls(camera, renderer.domElement)
 
-    var axes = new THREE.AxesHelper(10)
-    scene.add(axes)
-    var grid = new THREE.GridHelper()
-    scene.add(grid)
+    var scene = new THREE.Scene()
+    
+    //scene.background = new THREE.Color(0x5f6661)
+    var camera = new THREE.PerspectiveCamera( 60, (CANVAS_WIDTH / CANVAS_HEIGHT), 1, 1000 )
+    var renderer = new THREE.WebGLRenderer()
+    
+    //console.log(autoRotate);
+    var controls = new THREE.OrbitControls(camera, renderer.domElement)
+    
+
+    const loader = new THREE.TextureLoader();
+    loader.load('./img/fundoPara3d.jpg' , function(texture)
+            {
+             scene.background = texture;  
+            });
+
+    
 
     renderer.toneMapping = THREE.ReinhardToneMapping
     renderer.toneMappingExposure = 1.8
@@ -44,10 +57,10 @@ function beginThreeJs(){
     canvas.appendChild(renderer.domElement);
 
 
-    camera.position.x = -5
-    camera.position.y = 8
-    camera.position.z = 13
-    camera.lookAt(0,2,0)
+    camera.position.x = 0
+    camera.position.y = 20
+    camera.position.z = 17
+    camera.lookAt(0,0,0)
 
     new THREE.GLTFLoader().load(
         'models/desk2022.gltf',
@@ -64,22 +77,71 @@ function beginThreeJs(){
     }
     )
 
-    addLights()
-animate()
+    
+    if(hasPanel!=true){
+        var obj = {
+        
+
+            light: 6.0,
+            rotate: true,
+            openDoor: false,
+            openDrawers: false,
+    
+            height: 10,
+            noiseStrength: 10.2,
+            growthSpeed: 0.2,
+    
+            
+        };
+    
+        var gui = new dat.gui.GUI();
+    
+        gui.remember(obj);
+    
+       
+    
+        gui.add(obj, 'light').min(-10).max(10).step(0.25);
+        //gui.add(obj, 'height').step(5); // Increment amount
+    
+        // Choose from accepted values
+        
+    
+        // Choose from named values
+        gui.add(obj, 'rotate');
+        gui.add(obj, 'openDoor');
+        gui.add(obj, 'openDrawers');
+        hasPanel = true  
+        autoRotate = obj.speed; 
+    
+        
+        
+    
+    }
+
+    var hemilight = new THREE.HemisphereLight(0xffeeb1, 0x292619, 4)
+    scene.add(hemilight)
+
+    var spotLight = new THREE.SpotLight(0xFFFFFF, 4)
+    spotLight.castShadow = true
+    scene.add(spotLight) 
+    
+    //addLights()
+    animate()
 
 function animate() {
     requestAnimationFrame( animate )
     renderer.render( scene, camera )
+    //console.log("veio");
+    var autoRotate = obj.rotate;
+    var lightPosition = obj.light;
+    
+        controls.autoRotate=autoRotate;
+        spotLight.intensity = lightPosition;
+        //console.log(controls.autoRotate);
+        controls.update();
+    
 }
 
-function addLights(){
-    var hemilight = new THREE.HemisphereLight(0xffeeb1, 0x292619, 4)
-    scene.add(hemilight)
-
-    var spotLight = new THREE.SpotLight(0xFFA95C, 4)
-    spotLight.castShadow = true
-    scene.add(spotLight)  
-}
 
 }
 //document.getElementById("img-container").offsetWidth
